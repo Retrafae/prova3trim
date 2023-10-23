7f4cba6aef9c0fc046bc7d4d804803d401dcfe4d
Retrafae <130366200+Retrafae@users.noreply.github.com>
2023-10-23 11:34:13 -0300

    Add files via upload

---
+++ frontEnd3trimLeoncio-main/calculo.js
@@ -0,0 +1,16 @@
+function Calcular(){
+
+    let idadeCanina = prompt("Qual a idade do cachorrinho?");
+    let idadecahuma = idadeCanina * 7
+ 
+    let mostraResultado = document.querySelector(".mostraResultado")
+    mostraResultado.textContent = "Resultado: a idade humana do cachorrinho é " + idadecahuma;
+
+    if (idadecahuma < 11) alert("Seu Dog é uma criança")
+    if (idadecahuma > 59) alert("Seu Dog é um idoso")
+
+
+    //BRO ESTOU PERDIDO NÃO TA FAZENDO SOL, VOCÊ PODE ME AJUDAR A TERMINAR ESSA CALCULADORA PARA MEU FILHO??
+    //POR FAVOR BRO TERMINE PARA MIM GANG GANG GANG 777 FERNANDX CLOATHING
+    // CONTRATA ALGUEM NO MEU LUGAR ENTÃO CARALHO, NEM SENDO PAGO EU TO
+}
